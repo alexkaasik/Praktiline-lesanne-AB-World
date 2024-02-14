5975fbaa131bf5512bc7b7f6f7cfc9bcd6c54ed7
alexkaasik <aleks.kaasik@outlook.com>
2024-02-14 10:34:24 +0200

    added work 18

--- AleksanderKaasik_Praktiline.sql
+++ AleksanderKaasik_Praktiline.sql
@@ -32,8 +32,9 @@ SELECT Name, Population FROM `country` where Region like "%Europe%" ORDER by Pop
 --16 Cуммарное число жителей стран Европы и суммарную площадь её государств (730 074 600, 23 049 133.9)
 SELECT sum(Population), sum(SurfaceArea) FROM `country` where Continent like "%Europe%"; 
 --17  Число стран, расположенных не в Антарктике (234 записи)
-SELECT Name FROM `country` where Name != "Antarctica"; 
+SELECT Name FROM `country` where Region != "Antarctica"; 
 --18 Число стран, где главой правительства является Елизавета II (Elisabeth II), суммарное число жителей этих стран.  (35 стран, 122 872 550 человек)
+SELECT COUNT(Name), sum(Population) FROM `country` where HeadOfState like "Elisabeth II"; 
 --19 Число стран, наибольшее и наименьшее число жителей стран Полинезии (Polynesia) (10 стран, 235 000 человек, 50 человек)
 SELECT count(Name), max(Population), min(Population) FROM `country` where Region = "Polynesia";
 --20 Первые 5% списка стран мира наименьшей площади
